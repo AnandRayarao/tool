35b2c36b8d27aef5a643ce2874e9edcb94c99e17
rayar <rayarao.anand@gmail.com>
2020-04-20 23:26:57 +0530

    second

--- registration/views.py
+++ registration/views.py
@@ -1,10 +1,11 @@
+from django.contrib.auth.models import User
 from django.forms import forms
 from django.http import HttpResponse
 from django.shortcuts import render
 from django import forms
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UsernameField
 
 
 # Create your views here.
@@ -14,28 +15,42 @@ class logiForm(forms.Form):
         password = forms.CharField(label="Password",widget=forms.PasswordInput)
 
 class MYUserCreationForm(UserCreationForm):
+    first_name = forms.CharField(
+        label=("First Name"),
+        strip=False,
 
+    )
+    last_name = forms.CharField(
+        label=("Last Name"),
+        strip=False,
+
+    )
+    email = forms.CharField(
+        label=("Email"),
+        strip=False,
+
+    )
 
 def register(request):
     if request.user.is_authenticated:
         return redirect('/')
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = MYUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=password)
-            print(user, "12345")
             return redirect('/')
         else:
             return render(request, 'register.html', {'form': form})
     else:
-        form = UserCreationForm()
+        form = MYUserCreationForm()
         return render(request, 'register.html', {'form': form})
 
 def login(request):
     form = logiForm()
     if request.method == "POST":
-
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        authenticate(request,username,password)
     return render(request, "login.html" , {'form':form})
