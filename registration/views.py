43e439b37bee1ce80897911c3b662ecf8fe0121e
oc-arayarao <anand.rayarao@creditsafe.co.in>
2020-04-28 17:35:49 +0530

    register and login

--- registration/views.py
+++ registration/views.py
@@ -4,32 +4,25 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django import forms
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm, UsernameField
 
 
 # Create your views here.
 
 class logiForm(forms.Form):
-        username = forms.CharField(label='Email', max_length=100)
+        username = forms.CharField(label='username', max_length=100)
         password = forms.CharField(label="Password",widget=forms.PasswordInput)
 
-class MYUserCreationForm(UserCreationForm):
-    first_name = forms.CharField(
-        label=("First Name"),
-        strip=False,
+class MYUserCreationForm(forms.Form):
+    firstname = forms.CharField(label='Firstname', max_length=100)
+    lastname = forms.CharField(label='Lastname', max_length=100)
+    username = forms.CharField(label='Username', max_length=100)
+    email = forms.CharField(label='Email',widget=forms.EmailInput)
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password1 = forms.CharField(label="Conform password",
+                                widget=forms.PasswordInput)
 
-    )
-    last_name = forms.CharField(
-        label=("Last Name"),
-        strip=False,
-
-    )
-    email = forms.CharField(
-        label=("Email"),
-        strip=False,
-
-    )
 
 def register(request):
     if request.user.is_authenticated:
@@ -37,20 +30,35 @@ def register(request):
     if request.method == 'POST':
         form = MYUserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
-            return redirect('/')
-        else:
-            return render(request, 'register.html', {'form': form})
+            print(form.cleaned_data['username'],
+                                            form.cleaned_data['email'],
+                                            form.cleaned_data['password'])
+            user = User.objects.create_user(form.cleaned_data['username'],
+                                            form.cleaned_data['email'],
+                                            form.cleaned_data['password'])
+            user.save()
+
+            return redirect("/user/login")
     else:
         form = MYUserCreationForm()
         return render(request, 'register.html', {'form': form})
 
-def login(request):
-    form = logiForm()
+def userlogin(request):
+
     if request.method == "POST":
-        username = form.cleaned_data.get('username')
-        password = form.cleaned_data.get('password1')
-        authenticate(request,username,password)
-    return render(request, "login.html" , {'form':form})
+        form = logiForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username = username, password = password)
+            if user is not None:
+             login(request, user)
+             return redirect('/')
+    else:
+     form = logiForm()
+     return render(request, "login.html" , {'form':form})
+
+def userlogout(request):
+
+    logout(request)
+    return redirect("/")
